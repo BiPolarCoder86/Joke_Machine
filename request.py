f962b4d4f947f747d126975b9b50085fabb00926
Derek Underwood <biploarcoder@outlook.com>
2023-04-01 17:36:07 -0500

    Initial Commit
    Basic Working structure. Only pulls 'joke' in and not 'setup'. Need to correct within list.
    
    Also need to learn how to refresh API after each joke

--- request.py
+++ request.py
@@ -5,19 +5,42 @@ import random
 
 response = requests.get("https://v2.jokeapi.dev/joke/Programming")
 data = response.text
-joke_form = json.loads(data)
-#print("Joke", parse_json)
-print(joke_form['joke'])
+parse_json = json.loads(data)
 #print(response.status_code)
 
-#print(response.json())
-#Response form of Dictionary
+
+print("Welcome to the joke machine: ")
+print()
+
+while True:
+    user_input = input("Would you like to hear a joke? ")
+    if user_input.upper() == "Y":
+        print()
+        #print("Joke", parse_json)
+        print(parse_json['joke'])
+        print()
+    elif user_input.upper() == "N":
+        print()
+        print("Alright, talk to you later.")
+        print()
+        break
+    else:
+        break
+
+    
+    #Response form of Dictionary
     #type = single                   ----twopart (later) 
-    #joke                            ----(setup)
-    #                                ----delivery
+    #joke                            ----(setup)                           ----delivery
 
+#data = response.text
+#parse_json = json.loads(data)
+#print("Joke", parse_json)
+#print(joke_form['joke'])
 
-#keys_list = list(parse_json.keys())
+#keys_list = list(parse_json.values())
 #print(keys_list) 
+
+
+
 
 
